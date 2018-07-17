7319348111f151fa0f7a72c613bf499bdafd0029
Jonathan Luu <jonathanluu0@gmail.com>
2018-07-17 12:02:48 -0700

    WE GOT STARTED

--- js/app.js
+++ js/app.js
@@ -9,5 +9,32 @@ $(document).ready(function() {
     type: 'GET',
     url: 'https://api.themoviedb.org/3/movie/550?api_key=68a406cb55230475845b91eb462bdcff',
     success: onSuccess
-  })
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/209112/trailers?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
+    success: onSuccess
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/209112/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
+    success: onSuccess
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
+    success: onSuccess
+  });
+  https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
+});
+
+function onSuccess(response){
+  console.log(response.youtube[0].source)
+}
+
+function mySuperSweetFunctionWithALongName(){
+
 }
