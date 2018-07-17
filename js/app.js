ebbf090116acee79392aca374fd8c47c13440c6a
Jonathan Luu <jonathanluu0@gmail.com>
2018-07-17 13:29:50 -0700

    ajax objects

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
     console.log(response)
   }
 
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.themoviedb.org/3/search/movie/?api_key=68a406cb55230475845b91eb462bdcff&query=batman',
+    success: onSuccess
+  });
+
+
 
   $.ajax({
     type: 'GET',
@@ -11,11 +18,15 @@ $(document).ready(function() {
     success: onSuccess
   });
 
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.themoviedb.org/3/movie/209112/trailers?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
-    success: onSuccess
-  });
+
+  function getTrailers(movieId){
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.themoviedb.org/3/movie/' + movieId + '/trailers?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
+      success: onSuccess
+    });
+  }
+
 
   $.ajax({
     type: 'GET',
@@ -28,12 +39,13 @@ $(document).ready(function() {
     url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed',
     success: onSuccess
   });
-  https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed
+  function onSuccess(response){
+    // console.log(response.youtube[0].source)
+    getTrailers(response.id)
+
+  }
 });
 
-function onSuccess(response){
-  console.log(response.youtube[0].source)
-}
 
 function mySuperSweetFunctionWithALongName(){
 
